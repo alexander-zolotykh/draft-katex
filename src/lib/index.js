4f0dc4125a9fd7b890173e8aa7026e7ce90a8b0f
alex.zolotyh <alex.zolotyh@omega-r.com>
2019-03-05 16:30:02 +0300

    code style

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,158 +1,158 @@
 import {
-  EditorState,
-  RichUtils,
+    EditorState,
+    RichUtils,
 } from 'draft-js';
 import decorateComponentWithProps from 'decorate-component-with-props';
 import 'katex/dist/katex.css';
 import TeXBlock from './components/TeXBlock';
 import {
-  insertTeXBlock,
-  removeTeXBlock
+    insertTeXBlock,
+    removeTeXBlock,
 } from './modifiers';
 import InsertButton from './components/InsertKatexButton';
 
 import styles from './styles.module.css';
 
 function noopTranslator(tex) {
-  return tex;
+    return tex;
 }
 
 const blockTypes = {
-  ATOMIC: 'atomic'
+    ATOMIC: 'atomic',
 };
 
 const entityTypes = {
-  KATEX: 'KateX',
+    KATEX: 'KateX',
 };
 
 const defaultTheme = {
-  ...styles,
+    ...styles,
 };
 
 export default (config = {}) => {
-  const theme = Object.assign(defaultTheme, config.theme || {});
-  const insertContent = config.insertContent || 'Ω';
-  const doneContent = config.doneContent || {
-    valid: 'Done',
-    invalid: 'Invalid TeX',
-  };
-  const removeContent = config.removeContent || 'Remove';
-  const cancelContent = config.cancelContent || 'Cancel';
-  const translator = config.translator || noopTranslator;
-  const {
-    katex
-  } = config;
+    const theme = Object.assign(defaultTheme, config.theme || {});
+    const insertContent = config.insertContent || 'Ω';
+    const doneContent = config.doneContent || {
+        valid: 'Done',
+        invalid: 'Invalid TeX',
+    };
+    const removeContent = config.removeContent || 'Remove';
+    const cancelContent = config.cancelContent || 'Cancel';
+    const translator = config.translator || noopTranslator;
+    const {
+        katex,
+    } = config;
 
-  if (!katex || !katex.render) {
-    throw new Error('Invalid katex plugin provided!');
-  }
+    if (!katex || !katex.render) {
+        throw new Error('Invalid katex plugin provided!');
+    }
 
-  const store = {
-    editorStateBeforeInsertFormula: undefined,
-    getEditorState: undefined,
-    setEditorState: undefined,
-    getReadOnly: undefined,
-    setReadOnly: undefined,
-    onChange: undefined,
-    openImmediately: false,
-  };
+    const store = {
+        editorStateBeforeInsertFormula: undefined,
+        getEditorState: undefined,
+        setEditorState: undefined,
+        getReadOnly: undefined,
+        setReadOnly: undefined,
+        onChange: undefined,
+        openImmediately: false,
+    };
 
-  const liveTeXEdits = new Map();
+    const liveTeXEdits = new Map();
 
-  const component = decorateComponentWithProps(TeXBlock, {
-    theme,
-    store,
-    doneContent,
-    removeContent,
-    cancelContent,
-    translator,
-    katex,
-    MathInput: config.MathInput,
-  });
+    const component = decorateComponentWithProps(TeXBlock, {
+        theme,
+        store,
+        doneContent,
+        removeContent,
+        cancelContent,
+        translator,
+        katex,
+        MathInput: config.MathInput,
+    });
 
-  const insertFormula = (formula, openImmediately = false) => {
-    const currentEditorState = store.getEditorState();
-    const currentInlineStyle = currentEditorState.getCurrentInlineStyle();
+    const insertFormula = (formula, openImmediately = false) => {
+        const currentEditorState = store.getEditorState();
+        const currentInlineStyle = currentEditorState.getCurrentInlineStyle();
 
-    store.editorStateBeforeInsertFormula = currentEditorState;
-    store.openImmediately = openImmediately;
+        store.editorStateBeforeInsertFormula = currentEditorState;
+        store.openImmediately = openImmediately;
 
-    let nextEditorState = insertTeXBlock(store.getEditorState(), translator, formula);
+        let nextEditorState = insertTeXBlock(store.getEditorState(), translator, formula);
 
-    nextEditorState = currentInlineStyle.reduce(
-      (state, style) => RichUtils.toggleInlineStyle(state, style),
-      nextEditorState,
-    );
+        nextEditorState = currentInlineStyle.reduce(
+            (state, style) => RichUtils.toggleInlineStyle(state, style),
+            nextEditorState,
+        );
 
-    store.setEditorState(nextEditorState);
-  };
+        store.setEditorState(nextEditorState);
+    };
 
-  return {
-    initialize: ({
-      getEditorState,
-      setEditorState,
-      getReadOnly,
-      setReadOnly
-    }) => {
-      store.getEditorState = getEditorState;
-      store.setEditorState = setEditorState;
-      store.getReadOnly = getReadOnly;
-      store.setReadOnly = setReadOnly;
-    },
+    return {
+        initialize: ({
+                         getEditorState,
+                         setEditorState,
+                         getReadOnly,
+                         setReadOnly,
+                     }) => {
+            store.getEditorState = getEditorState;
+            store.setEditorState = setEditorState;
+            store.getReadOnly = getReadOnly;
+            store.setReadOnly = setReadOnly;
+        },
 
-    blockRendererFn: block => {
-      if (block.getType() === blockTypes.ATOMIC) {
-        const entity = store
-          .getEditorState()
-          .getCurrentContent()
-          .getEntity(block.getEntityAt(0));
-        const type = entity.getType();
+        blockRendererFn: block => {
+            if (block.getType() === blockTypes.ATOMIC) {
+                const entity = store
+                    .getEditorState()
+                    .getCurrentContent()
+                    .getEntity(block.getEntityAt(0));
+                const type = entity.getType();
 
-        if (type === entityTypes.KATEX) {
-          return {
-            component,
-            editable: false,
-            props: {
-              onStartEdit: blockKey => {
-                liveTeXEdits.set(blockKey, true);
-                store.setReadOnly(liveTeXEdits.size);
-              },
+                if (type === entityTypes.KATEX) {
+                    return {
+                        component,
+                        editable: false,
+                        props: {
+                            onStartEdit: blockKey => {
+                                liveTeXEdits.set(blockKey, true);
+                                store.setReadOnly(liveTeXEdits.size);
+                            },
 
-              onFinishEdit: (blockKey, newEditorState) => {
-                liveTeXEdits.delete(blockKey);
-                store.setReadOnly(liveTeXEdits.size);
-                store.setEditorState(EditorState.forceSelection(newEditorState, newEditorState.getSelection()));
-              },
+                            onFinishEdit: (blockKey, newEditorState) => {
+                                liveTeXEdits.delete(blockKey);
+                                store.setReadOnly(liveTeXEdits.size);
+                                store.setEditorState(EditorState.forceSelection(newEditorState, newEditorState.getSelection()));
+                            },
 
-              onRemove: blockKey => {
-                liveTeXEdits.delete(blockKey);
-                store.setReadOnly(liveTeXEdits.size);
-                let nextEditorState;
-                if (store.editorStateBeforeInsertFormula) {
-                  nextEditorState = store.editorStateBeforeInsertFormula;
-                  store.editorStateBeforeInsertFormula = null;
-                } else {
-                  const editorState = store.getEditorState();
-                  nextEditorState = removeTeXBlock(editorState, blockKey);
+                            onRemove: blockKey => {
+                                liveTeXEdits.delete(blockKey);
+                                store.setReadOnly(liveTeXEdits.size);
+                                let nextEditorState;
+                                if (store.editorStateBeforeInsertFormula) {
+                                    nextEditorState = store.editorStateBeforeInsertFormula;
+                                    store.editorStateBeforeInsertFormula = null;
+                                } else {
+                                    const editorState = store.getEditorState();
+                                    nextEditorState = removeTeXBlock(editorState, blockKey);
+                                }
+                                store.setEditorState(nextEditorState);
+                            },
+
+                            onCancel: () => {
+                                store.setEditorState(store.getEditorState());
+                            },
+                        },
+                    };
                 }
-                store.setEditorState(nextEditorState);
-              },
-
-              onCancel: () => {
-                store.setEditorState(store.getEditorState());
-              },
-            },
-          };
-        }
-      }
-      return null;
-    },
-    InsertButton: decorateComponentWithProps(InsertButton, {
-      theme,
-      store,
-      translator,
-      defaultContent: insertContent,
-    }),
-    insertFormula,
-  };
+            }
+            return null;
+        },
+        InsertButton: decorateComponentWithProps(InsertButton, {
+            theme,
+            store,
+            translator,
+            defaultContent: insertContent,
+        }),
+        insertFormula,
+    };
 };
