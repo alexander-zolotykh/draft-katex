8d4b98276ff4f8cf9df120b96db203ff0491f467
alex.zolotyh <alex.zolotyh@omega-r.com>
2019-03-14 15:10:07 +0300

    code style

--- src/lib/modifiers/removeTeXBlock.js
+++ src/lib/modifiers/removeTeXBlock.js
@@ -1,45 +1,47 @@
 import {
-  Modifier,
-  EditorState,
-  SelectionState
+    Modifier,
+    EditorState,
+    SelectionState
 } from 'draft-js';
 
 export default (editorState, blockKey) => {
-  let content = editorState.getCurrentContent();
-  const newSelection = new SelectionState({
-    anchorKey: blockKey,
-    anchorOffset: 0,
-    focusKey: blockKey,
-    focusOffset: 0,
-  });
+    let content = editorState.getCurrentContent();
+    const newSelection = new SelectionState({
+        anchorKey: blockKey,
+        anchorOffset: 0,
+        focusKey: blockKey,
+        focusOffset: 0,
+    });
 
-  const beforeKey = content.getKeyBefore(blockKey);
-  const afterKey = content.getKeyAfter(blockKey);
-  const beforeBlock = content.getBlockForKey(beforeKey);
-  const afterBlock = content.getBlockForKey(afterKey);
+    const beforeKey = content.getKeyBefore(blockKey);
+    const afterKey = content.getKeyAfter(blockKey);
+    const beforeBlock = content.getBlockForKey(beforeKey);
+    const afterBlock = content.getBlockForKey(afterKey);
 
-  const isBeforeBlockEmpty = beforeBlock.getLength() === 0;
-  const isAfterBlockEmpty = afterBlock.getLength() === 0;
+    const isBeforeBlockEmpty = beforeBlock.getLength() === 0;
+    const isAfterBlockEmpty = afterBlock.getLength() === 0;
 
-  const fromKey = isBeforeBlockEmpty ? beforeKey : blockKey;
-  const toKey = isAfterBlockEmpty ? afterKey : blockKey;
+    const fromKey = isBeforeBlockEmpty ? beforeKey : blockKey;
+    const toKey = isAfterBlockEmpty ? afterKey : blockKey;
 
-  const targetRange = new SelectionState({
-    anchorKey: fromKey,
-    anchorOffset: 0,
-    focusKey: toKey,
-    focusOffset: 0,
-  });
+    const targetRange = new SelectionState({
+        anchorKey: fromKey,
+        anchorOffset: 0,
+        focusKey: toKey,
+        focusOffset: 0,
+    });
 
-  // change the blocktype and remove the characterList entry with the sticker
-  content = Modifier.setBlockType(
-    content,
-    targetRange,
-    'unstyled'
-  );
-  content = Modifier.removeRange(content, targetRange, 'backward');
+    // change the blocktype and remove the characterList entry with the sticker
+    content = Modifier.setBlockType(
+        content,
+        targetRange,
+        'unstyled'
+    );
 
-  // force to new selection
-  const newState = EditorState.push(editorState, content, 'remove-range');
-  return EditorState.forceSelection(newState, newSelection);
+    content = Modifier.removeRange(content, targetRange, 'backward');
+
+    // force to new selection
+    const newState = EditorState.push(editorState, content, 'remove-range');
+
+    return EditorState.forceSelection(newState, newSelection);
 };
